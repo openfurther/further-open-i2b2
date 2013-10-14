717b9fc758986a5764c0395beb11c1655cdf1611
Dustin Schultz <dustin.schultz@utah.edu>
2013-10-14 09:39:24 -0600

    Replaced QUnit with XMLDiff and updated package for ResultTYpe.

--- i2b2-hook/i2b2-hook-further/src/test/java/edu/utah/further/i2b2/hook/further/UTestI2b2FurtherTo.java
+++ i2b2-hook/i2b2-hook-further/src/test/java/edu/utah/further/i2b2/hook/further/UTestI2b2FurtherTo.java
@@ -16,9 +16,7 @@
 package edu.utah.further.i2b2.hook.further;
 
 import static edu.utah.further.core.api.collections.CollectionUtil.newList;
-import static edu.utah.further.core.qunit.runner.XmlAssertion.xmlAssertion;
-import static edu.utah.further.fqe.ds.api.results.ResultType.INTERSECTION;
-import static edu.utah.further.fqe.ds.api.results.ResultType.SUM;
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -29,20 +27,25 @@ import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
+import org.xml.sax.SAXException;
 
 import com.sun.xml.bind.api.JAXBRIContext;
 
 import edu.utah.further.core.api.constant.Strings;
 import edu.utah.further.core.api.time.TimeService;
 import edu.utah.further.core.api.xml.XmlService;
-import edu.utah.further.core.qunit.runner.XmlAssertion;
+import edu.utah.further.core.test.annotation.UnitTest;
 import edu.utah.further.core.util.io.IoUtil;
 import edu.utah.further.core.xml.jaxb.JaxbConfigurationFactoryBean;
 import edu.utah.further.core.xml.jaxb.XmlServiceImpl;
 import edu.utah.further.fqe.ds.api.domain.QueryState;
+import edu.utah.further.fqe.ds.api.service.results.ResultType;
 import edu.utah.further.fqe.ds.api.to.QueryContextTo;
 import edu.utah.further.fqe.ds.api.to.QueryContextToImpl;
 import edu.utah.further.fqe.ds.api.util.FqeDsQueryContextUtil;
@@ -51,25 +54,25 @@ import edu.utah.further.i2b2.hook.further.domain.I2b2FurtherJoinResult;
 import edu.utah.further.i2b2.hook.further.domain.I2b2FurtherQueryResultTo;
 import edu.utah.further.i2b2.hook.further.service.FurtherServices;
 import edu.utah.further.i2b2.hook.further.service.FurtherServicesFactory;
-import edu.utah.further.core.test.annotation.UnitTest;
 
 /**
  * Unit tests of I2b2 request and response FURTHeR transfer objects.
  * <p>
- * -----------------------------------------------------------------------------------<br>
+ * ----------------------------------------------------------------------------
+ * -------<br>
  * (c) 2008-2013 FURTHeR Project, Health Sciences IT, University of Utah<br>
  * Contact: {@code <further@utah.edu>}<br>
  * Biomedical Informatics, 26 South 2000 East<br>
  * Room 5775 HSEB, Salt Lake City, UT 84112<br>
  * Day Phone: 1-801-581-4080<br>
- * -----------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * -------
  * 
  * @author Oren E. Livne {@code <oren.livne@utah.edu>}
  * @version Jan 6, 2011
  */
 @UnitTest
-public final class UTestI2b2FurtherTo
-{
+public final class UTestI2b2FurtherTo {
 	// ========================= CONSTANTS =================================
 
 	/**
@@ -81,7 +84,7 @@ public final class UTestI2b2FurtherTo
 	 * Test file names.
 	 */
 	private static final String FURTHER_QUERY_RESULT_XML = "further-query-result.xml";
-	
+
 	private static final String FURTHER_QUERY_RESULT_MASKED_XML = "further-query-result-masked.xml";
 
 	/**
@@ -101,24 +104,27 @@ public final class UTestI2b2FurtherTo
 	 */
 	private final FurtherServices furtherServices = FurtherServicesFactory
 			.newFurtherServicesInstance();
-	
+
 	/**
 	 * Stale date & time for the test QC.
 	 */
 	private Date staleDateTime;
-	
+
 	/**
 	 * The minimum number of results, otherwise masked
 	 */
 	@SuppressWarnings("boxing")
-	private static final Long resultMaskBoundary = 5l ;
+	private static final Long resultMaskBoundary = 5l;
 
 	// ========================= SETUP METHODS =============================
 
-	
 	@Before
-	public void setup()
-	{
+	public void setup() {
+		
+		XMLUnit.setIgnoreComments(true);
+		XMLUnit.setIgnoreWhitespace(true);
+		XMLUnit.setNormalizeWhitespace(true);
+		
 		/* Override the base */
 		JAXB_CONFIG.put(JAXBRIContext.DEFAULT_NAMESPACE_REMAP,
 				I2b2FurtherQueryResultTo.NAMESPACE);
@@ -134,19 +140,17 @@ public final class UTestI2b2FurtherTo
 	 * Marshal an i2b2 message FURTHeR query result section to XML.
 	 */
 	@Test
-	public void marshalFurtherQueryResult() throws Exception
-	{
+	public void marshalFurtherQueryResult() throws Exception {
 		marshallingTest(FURTHER_QUERY_RESULT_XML, newFurtherQueryResult());
 	}
-	
+
 	// ========================= TEST METHODS ==============================
 
 	/**
 	 * Marshal an i2b2 message FURTHeR query result section to XML.
 	 */
 	@Test
-	public void marshalFurtherQueryContextToResult() throws Exception
-	{
+	public void marshalFurtherQueryContextToResult() throws Exception {
 		marshallingMaskedQueryContextTest(FURTHER_QUERY_RESULT_MASKED_XML);
 	}
 
@@ -157,74 +161,76 @@ public final class UTestI2b2FurtherTo
 	 * @param entity
 	 * @throws IOException
 	 * @throws JAXBException
+	 * @throws SAXException
 	 */
 	private void marshallingTest(final String fileName,
-			final I2b2FurtherQueryResultTo entity) throws JAXBException, IOException
-	{
-		final String marshalled = furtherServices.marshalI2b2FurtherQueryResult(entity);
-		if (log.isDebugEnabled())
-		{
+			final I2b2FurtherQueryResultTo entity) throws JAXBException,
+			IOException, SAXException {
+		final String marshalled = furtherServices
+				.marshalI2b2FurtherQueryResult(entity);
+		if (log.isDebugEnabled()) {
 			log.debug("Marshalled:" + Strings.NEW_LINE_STRING + marshalled);
 		}
 		assertNotNull("Marshalling failed", marshalled);
 		final String expected = IoUtil.getResourceAsString(fileName);
-		xmlAssertion(XmlAssertion.Type.EXACT_MATCH)
-				.actualResourceString(marshalled)
-				.expectedResourceString(expected)
-				.stripNewLinesAndTabs(true)
-				.doAssert();
+
+		final DetailedDiff diff = new DetailedDiff(new Diff(expected,
+				marshalled));
+		assertTrue("XML is different" + diff.getAllDifferences(),
+				diff.similar());
 	}
-	
+
 	/**
 	 * @param fileName
 	 * @param entity
 	 * @throws IOException
 	 * @throws JAXBException
+	 * @throws SAXException
 	 */
-	private void marshallingMaskedQueryContextTest(final String i2b2FileName) throws JAXBException, IOException
-	{
+	private void marshallingMaskedQueryContextTest(final String i2b2FileName)
+			throws JAXBException, IOException, SAXException {
 		final QueryContextTo entity = newQueryContextToBasicWithResultViews();
 		final String marshalled = furtherServices.buildQueryResultXml(entity);
-		if (log.isDebugEnabled())
-		{
+		if (log.isDebugEnabled()) {
 			log.debug("Marshalled:" + Strings.NEW_LINE_STRING + marshalled);
 		}
 		assertNotNull("Marshalling failed", marshalled);
 		final String expected = IoUtil.getResourceAsString(i2b2FileName);
-		xmlAssertion(XmlAssertion.Type.EXACT_MATCH)
-				.actualResourceString(marshalled)
-				.expectedResourceString(expected)
-				.stripNewLinesAndTabs(true)
-				.doAssert();
+		final DetailedDiff diff = new DetailedDiff(new Diff(expected,
+				marshalled));
+		assertTrue("XML is different" + diff.getAllDifferences(),
+				diff.similar());
 	}
 
 	/**
 	 * @return
 	 */
-	private I2b2FurtherQueryResultTo newFurtherQueryResult()
-	{
+	private I2b2FurtherQueryResultTo newFurtherQueryResult() {
 		final I2b2FurtherQueryResultTo queryResult = new I2b2FurtherQueryResultTo();
 
-		queryResult.getDataSources().add(new I2b2FurtherDataSourceResult("UUEDW", 123));
-		queryResult.getDataSources().add(new I2b2FurtherDataSourceResult("UPDBL", 456));
+		queryResult.getDataSources().add(
+				new I2b2FurtherDataSourceResult("UUEDW", 123));
+		queryResult.getDataSources().add(
+				new I2b2FurtherDataSourceResult("UPDBL", 456));
 
 		queryResult.getJoins().add(new I2b2FurtherJoinResult("Sum", 789));
 		queryResult.getJoins().add(new I2b2FurtherJoinResult("Union", 500));
-		queryResult.getJoins().add(new I2b2FurtherJoinResult("Intersection", 100));
+		queryResult.getJoins().add(
+				new I2b2FurtherJoinResult("Intersection", 100));
 
 		return queryResult;
 	}
-	
+
 	/**
 	 * @param query
 	 * @return
 	 */
-	private QueryContextTo newQueryContextToBasic(final boolean withExecutionId)
-	{
+	private QueryContextTo newQueryContextToBasic(final boolean withExecutionId) {
 		final QueryContextTo queryContextTo = QueryContextToImpl.newInstance();
-		//Set this fixed for asserting the XML
+		// Set this fixed for asserting the XML
 		if (withExecutionId) {
-			queryContextTo.setExecutionId("3c0c8360-09f7-11e0-81e0-0800200c9a66");
+			queryContextTo
+					.setExecutionId("3c0c8360-09f7-11e0-81e0-0800200c9a66");
 		}
 		queryContextTo.setMinRespondingDataSources(2);
 		queryContextTo.setMaxRespondingDataSources(100);
@@ -237,16 +243,19 @@ public final class UTestI2b2FurtherTo
 	 * @return
 	 */
 	@SuppressWarnings("boxing")
-	private QueryContextTo newQueryContextToBasicWithResultViews()
-	{
+	private QueryContextTo newQueryContextToBasicWithResultViews() {
 		final QueryContextTo queryContextTo = newQueryContextToBasic(false);
 		final List<QueryContextTo> children = newList();
-		children.add(newChildQueryContextToWithResults("UUEDW", 8l, queryContextTo));
-		children.add(newChildQueryContextToWithResults("UPDBL", 3l, queryContextTo));
+		children.add(newChildQueryContextToWithResults("UUEDW", 8l,
+				queryContextTo));
+		children.add(newChildQueryContextToWithResults("UPDBL", 3l,
+				queryContextTo));
 		queryContextTo.addChildren(children);
-		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, SUM, null, 11l);
-		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, INTERSECTION, 1, 9l);
-		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, INTERSECTION, 2, 1l);
+		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, ResultType.SUM, null, 11l);
+		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, ResultType.INTERSECTION, 1,
+				9l);
+		FqeDsQueryContextUtil.addResultViewTo(queryContextTo, ResultType.INTERSECTION, 2,
+				1l);
 		queryContextTo.setStaleDateTime(staleDateTime);
 		return queryContextTo;
 	}
@@ -256,8 +265,8 @@ public final class UTestI2b2FurtherTo
 	 * @return
 	 */
 	@SuppressWarnings("boxing")
-	private QueryContextTo newChildQueryContextToWithResults(String dataSourceId, Long numRecords, QueryContextTo parent)
-	{
+	private QueryContextTo newChildQueryContextToWithResults(
+			String dataSourceId, Long numRecords, QueryContextTo parent) {
 		final QueryContextTo queryContextTo = QueryContextToImpl.newInstance();
 		queryContextTo.setNumRecords(numRecords);
 		queryContextTo.setState(QueryState.COMPLETED);
@@ -266,5 +275,5 @@ public final class UTestI2b2FurtherTo
 		queryContextTo.setParent(parent);
 		return queryContextTo;
 	}
-	
+
 }
